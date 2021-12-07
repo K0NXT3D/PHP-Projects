511893d12199d3a355cd5b525117adf29ab75b89
K0NXT3D <95604618+K0NXT3D@users.noreply.github.com>
2021-12-07 10:15:36 -0800

    Create php-nmap-form.php
    A cute little css free NMap PHP form.

---
+++ php-nmap-form.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+if(isset($_POST["target"])){
+ $target = $_POST["target"];
+ $output = shell_exec("nmap $target -T4 -A -v -sP");
+
+    echo "<pre>$output</pre>";
+    echo '<p><button onClick="window.location.href=window.location.href" type="submit"name="refresh">BACK</button></p>';
+
+} else{ ?>
+
+
+<form name="target" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+  <label for="target">Scan URL:</label><br>
+  <input type="text" id="target" name="target"> &nbsp;&nbsp;
+  <input type="submit" value="Submit">
+</form>
+
+ <?php } ?>
