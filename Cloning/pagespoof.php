d0a6be94fc422aa69f80a42a557ab48c41ae9ec6
Rob <95604618+K0NXT3D@users.noreply.github.com>
2021-12-22 12:42:14 -0800

    Update pagespoof.php
    Removed CSS

--- Cloning/pagespoof.php
+++ Cloning/pagespoof.php
@@ -1,51 +1,9 @@
 <head>
-<style>
-body {
-    padding:10px;
-    font-family: 'Roboto', sans-serif;
-    font-weight:900;
-    font-size:14px;
-    color: Lime;
-    background-image:linear-gradient(to right, #000,#990000);
-}
-
-input[type=text], label {
-    border-radius:10px;
-    padding:5px;
-    background-color: #000;
-    color: Lime;
-    transition: width 0.4s ease-in-out;
-}
-
-input[type=text]:focus {
-  width: 90%;
-}
-
-a {
-    text-decoration: none;
-}
-.title {
-    font-size:36px;
-    font-weight:900;
-    color:Gold;
-    text-shadow:0px 0px 4px #000;
-}
-
-.results {
-    margin: auto;
-    padding:15px;
-    width:80%;
-    background-image:linear-gradient(to right,#990000,#000);
-    border:2px solid #000;
-    border-radius:10px;
-    box-shadow:4px 4px 8px #000;
-}
-</style>
-</head>
  <title>Simple Page Spoofing</title>
+</head>
 <body>
- <div class="results">
-  <a class="title">Simple Page Spoofer</a>
+ <div>
+  <a>Simple Page Spoofer</a>
    <p><form name="target" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
     URL To Spoof:
      <input type="text" id="target" name="target" value=""> &nbsp;&nbsp;
